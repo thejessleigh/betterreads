fa9a33b26bdf491fa9e182a1b82582c6f42bdc26
thejessleigh <j.l.unrein@gmail.com>
2019-04-10 15:01:30 -0500

    Add API call mocked tests for review.

--- tests/review_test.py
+++ tests/review_test.py
@@ -1,53 +1,66 @@
+import json
+from unittest import mock
+
 import pytest
 
+from betterreads.client import GoodreadsClient
 from betterreads.review import GoodreadsReview
 
 
-@pytest.mark.skip(reason="Test suite pending rewrite.")
 class TestReview:
-    @classmethod
-    def setup_class(cls):
-        cls.review = cls.client.review("2")
+    @pytest.fixture
+    @mock.patch("betterreads.client.GoodreadsClient.request")
+    def test_review(self, mock_request):
+        client = GoodreadsClient("GOODREADS_KEY", "GOODREADS_SECRET")
 
-    def test_repr(self):
-        eq_(repr(self.review), "review [2]")
+        # Goodreads Group data pulled on 2019-04-09. Lightly modified for testing purposes
+        with open("tests/fixtures/review.json") as f:
+            review_response = f.read()
+            mock_request.return_value = json.loads(review_response)
+        return client.review(12345)
 
+    def test_repr(self, test_review):
+        assert repr(test_review) == "review [12345]"
+
+    @pytest.mark.skip(
+        reason="this is a test of client functionality. Move test when you get around to client_test.py"
+    )
     def test_recent_reviews(self):
         reviews = self.client.recent_reviews()
         ok_(all(isinstance(r, GoodreadsReview) for r in reviews))
 
-    def test_review_gid(self):
-        eq_(self.review.gid, "2")
+    def test_review_gid(self, test_review):
+        assert test_review.gid == "12345"
 
-    def test_review_book(self):
-        eq_(self.review.book["isbn"], "0517226952")
+    def test_review_book(self, test_review):
+        assert test_review.book["isbn"] == "0316767727"
 
-    def test_review_rating(self):
-        eq_(self.review.rating, "5")
+    def test_review_rating(self, test_review):
+        assert test_review.rating == "5"
 
-    def test_review_shelves(self):
-        eq_(len(self.review.shelves), 6)
+    def test_review_shelves(self, test_review):
+        assert len(test_review.shelves) == 1
 
-    def test_review_recommended_for(self):
-        eq_(self.review.recommended_for, None)
+    def test_review_recommended_for(self, test_review):
+        assert test_review.recommended_for is None
 
-    def test_review_recommended_by(self):
-        eq_(self.review.recommended_by, "Keaka")
+    def test_review_recommended_by(self, test_review):
+        assert test_review.recommended_by is None
 
-    def test_review_started_at(self):
-        eq_(self.review.started_at, None)
+    def test_review_started_at(self, test_review):
+        assert test_review.started_at == "Thu Aug 31 00:00:00 -0700 2000"
 
-    def test_review_read_at(self):
-        eq_(self.review.read_at, None)
+    def test_review_read_at(self, test_review):
+        assert test_review.read_at == "Fri Sep 01 00:00:00 -0700 2000"
 
-    def test_review_body(self):
-        eq_(self.review.body, None)
+    def test_review_body(self, test_review):
+        assert test_review.body == "These are some words in the review body"
 
-    def test_review_comments_count(self):
-        eq_(self.review.comments_count, "0")
+    def test_review_comments_count(self, test_review):
+        assert test_review.comments_count == "0"
 
-    def test_review_url(self):
-        eq_(self.review.url, "https://www.goodreads.com/review/show/2")
+    def test_review_url(self, test_review):
+        assert test_review.url == "https://www.goodreads.com/review/show/12345"
 
-    def test_review_owned(self):
-        eq_(self.review.owned, "1")
+    def test_review_owned(self, test_review):
+        assert test_review.owned == "0"
