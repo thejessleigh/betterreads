43f2fca37427de0494e2c664f7c3b83be5ed7f0c
Conrad Hughes <ConradHughes@users.noreply.github.com>
2020-05-19 14:59:58 +0100

    Add diskcache to client constructor.
    .. and move rate throttling in there too, for simplicity.

--- betterreads/request.py
+++ betterreads/request.py
@@ -1,5 +1,3 @@
-from ratelimit import limits, sleep_and_retry
-import requests
 import xmltodict
 import json
 
@@ -19,13 +17,12 @@ class GoodreadsRequest:
         self.params = query_dict
         self.params.update(client.query_dict)
         self.host = client.base_url
+        self.requests_get = client.requests_get
         self.path = path
         self.req_format = req_format
 
-    @sleep_and_retry
-    @limits(calls=1, period=1)
     def request(self):
-        resp = requests.get(self.host + self.path, params=self.params)
+        resp = self.requests_get(self.host + self.path, params=self.params)
         if resp.status_code != 200:
             raise GoodreadsRequestException(resp.reason, self.path)
         if self.req_format == "xml":
